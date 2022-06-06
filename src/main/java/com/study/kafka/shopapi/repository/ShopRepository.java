4219787e7e7550e5ff7880add83ea8183ae2dae6
eder.rodrigues <ederpr@gmail.com>
2022-06-06 07:06:01 -0300

    Adicacao da busca do produto pelo seu identificador

--- src/main/java/com/study/kafka/shopapi/repository/ShopRepository.java
+++ src/main/java/com/study/kafka/shopapi/repository/ShopRepository.java
@@ -4,5 +4,5 @@ import com.study.kafka.shopapi.model.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShopRepository extends JpaRepository<Shop, Long> {
-
+    Shop findByIdentifier(String identifier);
 }
